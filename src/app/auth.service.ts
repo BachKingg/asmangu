4ebca5a746d373728a27b174659a86152a246f67
Bach Nguyen <42868347+BachKingg@users.noreply.github.com>
2022-10-04 10:05:53 +0700

    update sort

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -3,31 +3,31 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import * as moment from 'moment';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  constructor(private _http: HttpClient) {}
+  login(username: string = '', password: string = '') {
+    const userInfo = { un: username, pw: password };
+    const headers = new HttpHeaders().set('Content-Type', 'application/json');
+    return this._http.post(
+      'http://localhost:3000/login',
+      JSON.stringify(userInfo),
+      { headers: headers, responseType: 'text' }
+    );
+  } //login
 
-    constructor(private _http: HttpClient) { }
-    login(username: string = '', password: string = '') {
-        const userInfo = { un: username, pw: password }
-        const headers = new HttpHeaders().set('Content-Type', 'application/json');
-        return this._http.post('http://localhost:3000/login'
-            , JSON.stringify(userInfo)
-            , { headers: headers, responseType: 'text' }
-        )
-    }//login
+  public daDangNhap() {
+    const str = localStorage.getItem('expires_at') || '';
+    if (str == '') return false; //chưa dn
+    const expiresAt = JSON.parse(str);
+    return moment().isBefore(moment(expiresAt));
+  }
 
-    public daDangNhap() {
-        const str = localStorage.getItem("expires_at") || "";
-        if (str == "") return false; //chưa dn    
-        const expiresAt = JSON.parse(str);
-        return moment().isBefore(moment(expiresAt));
-    } 
-    
-    thoat() {
-        localStorage.removeItem("id_token");
-        localStorage.removeItem("expires_at");
-        localStorage.removeItem("username");
-        localStorage.removeItem("userid");
-    }
+  thoat() {
+    localStorage.removeItem('id_token');
+    localStorage.removeItem('expires_at');
+    localStorage.removeItem('username');
+    localStorage.removeItem('userid');
+  }
 }
--- src/app/dangnhap/dangnhap.component.ts
+++ src/app/dangnhap/dangnhap.component.ts
@@ -4,41 +4,33 @@ import { AuthService } from '../auth.service';
 import * as moment from 'moment';
 
 @Component({
-    selector: 'app-dangnhap',
-    templateUrl: './dangnhap.component.html',
-    styleUrls: ['./dangnhap.component.css']
+  selector: 'app-dangnhap',
+  templateUrl: './dangnhap.component.html',
+  styleUrls: ['./dangnhap.component.css'],
 })
 export class DangnhapComponent implements OnInit {
+  constructor(private router: Router, private auth: AuthService) {}
 
-    constructor(
-        private router: Router,
-        private auth: AuthService,
+  ngOnInit(): void {}
 
-    ) { }
-
-    ngOnInit(): void {
-    }
-    
-    xulyDN(data: any) {
-        console.log(data);
-        this.auth.login(data.un, data.pw).subscribe(
-            (res) => {
-                var d = JSON.parse(res);
-                console.log('Đăng nhập thành công ', res);
-                const expiresAt = moment().add(d.expiresIn, 'second');
-                localStorage.setItem('id_token', d.idToken);
-                localStorage.setItem('username', data.un);
-                localStorage.setItem('userid', d.userId);
-                localStorage.setItem('expires_at', JSON.stringify(expiresAt.valueOf()));
-                this.router.navigateByUrl('/');
-            },
-            (error) => {
-                console.log('oops', error);
-                alert('Sai tài khoản hoặc mật khẩu!');
-                // this.router.navigateByUrl('/dangNhap');
-            }
-        );
-    }
-
-
+  xulyDN(data: any) {
+    console.log(data);
+    this.auth.login(data.un, data.pw).subscribe(
+      (res) => {
+        var d = JSON.parse(res);
+        console.log('Đăng nhập thành công ', res);
+        const expiresAt = moment().add(d.expiresIn, 'second');
+        localStorage.setItem('id_token', d.idToken);
+        localStorage.setItem('username', data.un);
+        localStorage.setItem('userid', d.userId);
+        localStorage.setItem('expires_at', JSON.stringify(expiresAt.valueOf()));
+        this.router.navigateByUrl('/');
+      },
+      (error) => {
+        console.log('oops', error);
+        alert('Sai tài khoản hoặc mật khẩu!');
+        // this.router.navigateByUrl('/dangNhap');
+      }
+    );
+  }
 }
